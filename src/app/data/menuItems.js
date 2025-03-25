8b196d51e7fc5f1f2e425ab972c9e4d5cec249ab
Ali Ibrahimi <alimadad.ibrahimi1116@gmail.com>
2025-03-26 02:12:45 +0330

    create watandar menu

---
+++ src/app/data/menuItems.js
@@ -0,0 +1,40 @@
+export const menuItems = [
+    {
+      id: 1,
+      name: {
+        fa: "کباب کوبیده",
+        ar: "كباب كوبيدة",
+        en: "Kebab Koobideh"
+      },
+      description: {
+        fa: "کباب کوبیده خوشمزه و لذیذ، پخته شده از گوشت تازه.",
+        ar: "كباب كوبيدة لذيذ مصنوع من لحم طازج.",
+        en: "Delicious Koobideh kebab made from fresh meat."
+      },
+      price: "300 AFN",
+      image: "public/food.pic.png",
+      weight: "300gr"
+    },
+  
+  ];
+  
+    export const translations = {
+      fa:{
+        menu: 'منو',
+        salad: 'سالاد',
+        drink: 'نوشیدنی',
+        food: 'غذا',
+      },
+      ar:{
+        menu: 'القائمة',
+        salad: 'سلطات',
+        drink: 'مشروبات',
+        food: 'أطعمة',
+      },
+      en:{
+        menu: 'Menu',
+        salad: 'Salad',
+        drink: 'Drink',
+        food: 'food',
+      },
+    }
