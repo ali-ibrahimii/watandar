07efdb5d448e46438a3056f2cc0b505e0dce4905
Ali Ibrahimi <alimadad.ibrahimi1116@gmail.com>
2025-03-29 04:39:57 +0330

    change the design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,12 @@ import SlideButton from "../../components/SlideButton"
 export default function Home() {
 
   return (
-    <div className="relative w-full h-screen bg-black">
-      <div className="absolute inset-0 flex flex-col items-center text-center">
-        <div className="relative">
-          <video 
-          playsInline autoPlay muted loop 
-          className="w-full h-screen inset-0"
-          src="background.mp4"></video>
-          <div className="absolute bg-gradient-to-l from-black to-transparent z-20"></div>
-        </div>
-        {/* <div>
-          <img className="w-full h-screen" src="bg.jpg" alt="background" />
-        </div> */}
-        <div className="absolute bottom-1/12">
+    <div className="relative w-full h-screen">
+      <div className="">
+        <img className="w-full h-screen" src="background.png" alt="" />
+        <img className="absolute top-5/12 left-1/2 w-10/12 transform -translate-x-1/2 -translate-y-1/2" src="main-bg.png" alt="" />
+
+        <div className="absolute top-10/12 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <SlideButton />
         </div>
       </div>
