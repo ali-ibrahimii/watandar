21af2dc635886ec7b925b299d3ee9295faa4552d
Ali Ibrahimi <alimadad.ibrahimi1116@gmail.com>
2025-04-10 13:37:53 +0330

    create slider

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,20 +1,14 @@
-import React from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { menuItems } from '@/app/data/menuItems';
-import { useLanguage } from '@/app/context/LanguageContext'
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-
-import '../src/app/style/style.css';
-
-// import required modules
-import { Pagination } from 'swiper/modules';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { menuItems } from "@/app/data/menuItems";
+import { useLanguage } from "@/app/context/LanguageContext";
+import "swiper/css";
+import "swiper/css/pagination";
+import "../src/app/style/style.css";
+import { Pagination } from "swiper/modules";
 
 export default function App() {
-   const {language} = useLanguage()
+  const { language } = useLanguage();
   return (
     <>
       <Swiper
@@ -22,17 +16,27 @@ export default function App() {
         spaceBetween={10}
         centeredSlides={true}
         loop
-        pagination={{
-          clickable: true,
-        }}
         modules={[Pagination]}
-        className="mySwiper"
+        className="mySwiper mt-10"
       >
+        {menuItems.map((items, index) => (
+          <SwiperSlide key={index} className="swiper-slide">
+            <img
+              src={items.image}
+              alt={items.name[language]}
+              className="swiper-slide-img"
+            />
 
-        {menuItems.map((items,index) => (
-          <SwiperSlide key={index} className='flex flex-col justify-center items-center'>
-            <h1 className='font-bold text-3xl'>{items.name[language]}</h1>
-            <p>{items.description[language]}</p>
+            {/* Content */}
+            <h1 className="text-white font-bold text-center mt-16">
+              {items.name[language]}
+            </h1>
+            <p className="text-sm text-gray-300 text-center mt-1">
+              {items.description[language]}
+            </p>
+            <p className="text-orange-400 font-bold text-center mt-2">
+              {items.price} T
+            </p>
           </SwiperSlide>
         ))}
       </Swiper>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 // import Navbar from "../../components/Navbar";
+import { useLanguage } from '@/app/context/LanguageContext'
+import LanguageSelector from '../../components/LanguageSelector';
 import MenuCard from "../../components/Menu";
 
 export default function Home() {
+  const {language} = useLanguage()
   return (
-    <div className="relative w-full h-screen">
+    <div className="w-full h-screen overflow-hidden">
+      <div className="w-full h-screen fixed top-0 left-0">
+        <img className="w-full h-full" src="background.jpg" alt="background" />  
+      </div>
+      <LanguageSelector />
       <MenuCard />
     </div>
   );
