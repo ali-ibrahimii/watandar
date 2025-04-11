28c26b404cc60fe4fafc83dd22b55fe351019502
Ali Ibrahimi <alimadad.ibrahimi1116@gmail.com>
2025-04-11 14:25:53 +0330

    create food, drink and salad list

---
+++ components/Salad.tsx
@@ -0,0 +1,25 @@
+import { useLanguage } from "@/app/context/LanguageContext";
+import { menuItems, translations } from "@/app/data/menuItems";
+import React from "react";
+
+const Salad = () => {
+  const { language } = useLanguage();
+  return (
+    <div className="flex flex-col items-start px-10 relative">
+      <div className="flex flex-col items-center">
+        <h1 className="text-white font-bold text-2xl mt-5">{translations[language].salad}</h1>
+      </div>
+      {menuItems.map((item) => (
+        <div key={item.id} className="flex justify-start mb-2 items-center  p-2 w-full h-15 bg-white rounded-full">
+          <img className="w-15" src={item.image} alt="" />
+          <div className="flex flex-col ml-2">
+            <h1>{item.name[language]}</h1>
+            <p>{item.description[language]}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Salad;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,33 @@
 "use client";
 
 
+import Drink from '../../components/Drink';
+import Food from '../../components/Food';
 import LanguageSelector from '../../components/LanguageSelector';
 import MenuCard from "../../components/Menu";
+import Navbar from '../../components/Navbar';
+import Salad from '../../components/Salad';
 
 export default function Home() {
   return (
-    <div className="w-full h-screen overflow-hidden">
+    <div className="w-full h-screen relative">
       <div className="w-full h-screen fixed top-0 left-0">
         <img className="w-full h-full" src="background.jpg" alt="background" />  
       </div>
-      <LanguageSelector />
-      <MenuCard />
+      <div className='w-full absolute top-0 left-0 flex flex-col justify-center items-center'>
+        <img className='w-20' src="logo.png" alt="logo" />
+        <h1 className='text-white font-bold text-2xl'>Watandar Restaurant</h1>
+        <Navbar />
+      </div>
+      <div className='z-[500]'>
+        <LanguageSelector />
+      </div>
+      <div className='mt-36'>
+        <MenuCard />
+      </div>
+      <Food />
+      <Drink />
+      <Salad />
     </div>
   );
 }
